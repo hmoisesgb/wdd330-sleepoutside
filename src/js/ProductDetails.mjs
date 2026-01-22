1bf853b48ebeb23ac406f65a5fc8625f2cc3bae5
hmoisesgb <h.moises.gb@gmail.com>
2026-01-22 16:24:00 -0600

    Cleaned product.js and created a ProductDetails module

---
+++ src/js/ProductDetails.mjs
@@ -0,0 +1,31 @@
+export default class ProductDetails {
+constructor(productId, dataSource){
+    this.productId = productId;
+    this.product = {};
+    this.dataSource = dataSource;
+}
+async init(){
+    this.product = await this.dataSource.findProductById(this.productId);
+    this.renderProductDetails();
+    document.getElementById("addToCart").addEventListener("click", this.addProductToCart.bind(this));
+}
+addProductToCart(){
+  const cartItems = getLocalStorage("so-cart") || []; // get cart array of items from local storage if null set to empty array
+  cartItems.push(this.product); // add the product to the cartItems array
+  setLocalStorage("so-cart", cartItems); // save updated cart array to local storage
+}
+renderProductDetails(){
+    productDetailsTemplate(this.product);
+}
+}
+
+function productDetailsTemplate(product) {
+    document.querySelector('h3').textContent = product.Brand.Name;
+    document.querySelector('h2').textContent = product.NameWithoutBrand;
+    document.getElementById('productImage').src = product.Image;
+    document.getElementById('productImage').alt = product.NameWithoutBrand;
+    document.getElementById('productPrice').textContent = product.FinalPrice;
+    document.getElementById('productColor').textContent = product.Colors[0].ColorName;
+    document.getElementById('productDesc').textContent = product.DescriptionHtmlSimple;
+    document.getElementById("addToCart").dataset.id = product.Id;
+}
--- src/js/product.js
+++ src/js/product.js
@@ -1,20 +1,9 @@
-import { setLocalStorage, getLocalStorage } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
+const productID = getParam("product");
 const dataSource = new ProductData("tents");
 
-function addProductToCart(product) {
-  const cartItems = getLocalStorage("so-cart") || []; // get cart array of items from local storage if null set to empty array
-  cartItems.push(product); // add the product to the cartItems array
-  setLocalStorage("so-cart", cartItems); // save updated cart array to local storage
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+const product = new ProductDetails(productID, dataSource);
+product.init();
